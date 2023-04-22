715817cd1cb37ef2270304dfc83297fa0bfaa676
shankar patil <shankar.patil@xrda3.com>
2023-04-22 13:38:46 +0530

    Assessment
    Write a MACRO function to multipy 2 float values, values will enter by user. Print the value upto 3 decimal.
    	Output Expected:
    		Enter 2 values: 2.5 <this will enter by user>
    		1.5
    		Multiplication of 2.5 and 1.5 is 3.750
    
    Signed-off-by: shankar patil <shankar.patil@xrda3.com>

---
+++ shankar_patil/micro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define  j (a*b)
+int main() {
+    float a,b;
+    printf("enter the value of a:");
+    scanf("%f",&a);
+    
+    printf("enter the value of b:");
+    scanf("%f",&b);
+    printf("%f",j);
+
+    return 0;
+}
